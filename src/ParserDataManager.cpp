7c886a883c9639f0022d4d6e93b735e998d9ae5a
Matthew D Nolan <104527166+Mattermania@users.noreply.github.com>
2024-02-23 19:36:56 -0700

    Update ParserDataManager.cpp

--- src/ParserDataManager.cpp
+++ src/ParserDataManager.cpp
@@ -1,8 +1,45 @@
 #include "ParserDataManager.h"
 #include "ParserTypes.h"
 
+#include <regex>
+
 namespace Parser
 {
+    // TODO: Check that the create_wire function produces proper output (namely that there aren't aren't duplicate names).
+    wire* ParserDataManager::create_wire(string name)
+    {
+        // Constructing the regular expression pattern to match names like "name_#"
+        std::regex pattern(name + "_\\d+");
+
+        // Count of wires with the same base name
+        int nameInc = 0;
+
+        // Loop through existing wires to find a unique name
+        for (size_t i = 0; i < wires.size(); ++i)
+        {
+            // If the name matches the base name or the pattern, increment the counter
+            if ((wires[i]->name == name) || (std::regex_search(wires[i]->name, pattern)))
+            {
+                nameInc += 1;
+            }
+        }
+
+        // If there are wires with the same base name, append an incremented number
+        if (nameInc > 0) {
+            name += "_" + std::to_string(nameInc);
+        }
+
+        // Create a new wire with the unique name
+        wire* newWire = new wire;
+        newWire->name = name;
+
+        // Add the new wire to the vector of wires
+        wires.push_back(newWire);
+
+        // Return a pointer to the new wire
+        return newWire;
+    }
+
     wire* ParserDataManager::find_wire(string name)
     {
         for (size_t i = 0; i < wires.size(); ++i)
