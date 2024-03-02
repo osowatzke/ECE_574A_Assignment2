d71d2805635e2ad84498930fd878e413f0634dff
Owen <osowatzke@gmail.com>
2024-03-02 16:00:04 -0700

    Removed duplicated component generator

--- src/DataPathGenerator.cpp
+++ src/DataPathGenerator.cpp
@@ -13,14 +13,14 @@ namespace DataPathGen
 DataPathGenerator::DataPathGenerator()
     : data_manager(DataManager())
     , file_parser(FileParser(&data_manager))
-    , implicit_component_generator(ImplicitComponentGenerator(&data_manager))
+    , component_generator(ImplicitComponentGenerator(&data_manager))
     , file_writer(FileWriter(&data_manager))
 {}
 
 void DataPathGenerator::run(string in_file_name, string out_file_name)
 {
     file_parser.run(in_file_name);
-    implicit_component_generator.run();
+    component_generator.run();
     file_writer.run(out_file_name);
     data_manager.print_wires();
     data_manager.print_components();
