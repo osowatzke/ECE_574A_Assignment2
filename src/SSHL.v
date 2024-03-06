94196b2960a1c08eeed43bac6eb516af4b0c3844
Kazi Safkat <safkat@arizona.edu>
2024-03-06 16:09:19 -0700

    Update SSHL.v

--- src/SSHL.v
+++ src/SSHL.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ns
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/05/2024 10:28:04 PM
-// Design Name: 
-// Module Name: SSHL
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module SSHL(a,sh_amt,d);
 
