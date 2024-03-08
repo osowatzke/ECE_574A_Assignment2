7596a8f30fd4056b4148212227239d640e802e1b
Kazi Safkat <safkat@arizona.edu>
2024-03-08 00:33:21 -0700

    Update ucircuit3.v
    Capitalized clock and reset

--- verilog_files/reference/ucircuit3.v
+++ verilog_files/reference/ucircuit3.v
@@ -1,8 +1,8 @@
 `timescale 1ns / 1ns
 
-module ucircuit3(a, b, c, clk, rst);
+module ucircuit3(a, b, c, Clk, Rst);
 
-        input clk, rst;
+        input Clk, Rst;
         input  [ 31:0] a;
         input  [ 15:0] b;
 
@@ -14,6 +14,6 @@ module ucircuit3(a, b, c, clk, rst);
         assign b_8 = b[7:0];
 
         ADD    #(.DATAWIDTH( 8)) ADD_1(a_8, b_8, cwire);                // cwire = a + b
-        REG    #(.DATAWIDTH( 8)) REG_1(c, cwire, clk, rst);             // c = cwire
+        REG    #(.DATAWIDTH( 8)) REG_1(c, cwire, Clk, Rst);             // c = cwire
 
 endmodule
