1790ad5ab9ab4bd85c7cdf6be04cf6dea68fbfd6
Owen <osowatzke@gmail.com>
2024-03-09 01:41:29 -0800

    Finish adding comments

--- src/DataManager.cpp
+++ src/DataManager.cpp
@@ -5,34 +5,34 @@
 
 namespace DataPathGen
 {
-    // Function returns a unique wire name 
-    string DataManager::get_unique_wire_name(string name)
-    {
-        // Count of wires with the same base name
-        int nameInc = -1;
-        bool isMatch = true;
+// Function returns a unique wire name 
+string DataManager::get_unique_wire_name(string name)
+{
+    // Count of wires with the same base name
+    int nameInc = -1;
+    bool isMatch = true;
 
-        // Loop through existing wires to find a unique name
-        while(isMatch) {
-            isMatch = false;
-            for (size_t i = 0; i < wires.size(); ++i)
+    // Loop through existing wires to find a unique name
+    while(isMatch) {
+        isMatch = false;
+        for (size_t i = 0; i < wires.size(); ++i)
+        {
+            // If the name matches the base name or the pattern, increment the counter
+            if (((nameInc < 0) && (wires[i]->name == name)) || ((nameInc >= 0) && (wires[i]->name == (name + "_" + to_string(nameInc)))))
             {
-                // If the name matches the base name or the pattern, increment the counter
-                if (((nameInc < 0) && (wires[i]->name == name)) || ((nameInc >= 0) && (wires[i]->name == (name + "_" + to_string(nameInc)))))
-                {
-                    isMatch = true;
-                    nameInc += 1;
-                }
+                isMatch = true;
+                nameInc += 1;
             }
         }
-
-        // If there are wires with the same base name, append an incremented number
-        if (nameInc >= 0) {
-            name += "_" + std::to_string(nameInc);
-        }
-
-        return name;
     }
+
+    // If there are wires with the same base name, append an incremented number
+    if (nameInc >= 0) {
+        name += "_" + std::to_string(nameInc);
+    }
+
+    return name;
+}
 
 // Function returns a pointer to a wire with the given name
 wire* DataManager::find_wire(string name)
