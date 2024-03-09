ec8e86b6de4e9b0e746c44fc0b0afbeee6113e1e
Owen <osowatzke@gmail.com>
2024-03-09 01:17:01 -0800

    Starting to add comments to file writer

--- src/CriticalPathCalculator.cpp
+++ src/CriticalPathCalculator.cpp
@@ -6,144 +6,144 @@
 
 namespace DataPathGen
 {
-    // Class constructor provides access to shared data manager
-    CriticalPathCalculator::CriticalPathCalculator(DataManager* data_manager)
-        : data_manager(data_manager) {};
+// Class constructor provides access to shared data manager
+CriticalPathCalculator::CriticalPathCalculator(DataManager* data_manager)
+    : data_manager(data_manager) {};
 
-    // Function runs critical path calculation
-    void CriticalPathCalculator::run()
+// Function runs critical path calculation
+void CriticalPathCalculator::run()
+{
+    outputCriticalPath();
+}
+
+// Function outputs the critical path
+void CriticalPathCalculator::outputCriticalPath()
+{
+    // Compute critical path
+    double criticalPath = computeCriticalPath();
+
+    // Output critical path
+    cout << "Critical Path : " << criticalPath << " ns" << endl;
+}
+
+// Function computs the critical path
+double CriticalPathCalculator::computeCriticalPath()
+{
+
+    // Initialize critical path
+    double criticalPath = 0.0;
+
+    // Loop through all the wires in the data manager
+    for (wire*& currWire : data_manager->wires)
     {
-        outputCriticalPath();
-    }
 
-    // Function outputs the critical path
-    void CriticalPathCalculator::outputCriticalPath()
-    {
-        // Compute critical path
-        double criticalPath = computeCriticalPath();
-
-        // Output critical path
-        cout << "Critical Path : " << criticalPath << " ns" << endl;
-    }
-
-    // Function computs the critical path
-    double CriticalPathCalculator::computeCriticalPath()
-    {
-
-        // Initialize critical path
-        double criticalPath = 0.0;
-
-        // Loop through all the wires in the data manager
-        for (wire*& currWire : data_manager->wires)
+        // Consider all input wires except clock and reset
+        if ((currWire->type == WireType::INPUT) && ((currWire->name != "Clk") && (currWire->name != "Rst")))
         {
+            // Compute maximum path delay starting at input
+            double pathDelay = computeCriticalPath(currWire);
 
-            // Consider all input wires except clock and reset
-            if ((currWire->type == WireType::INPUT) && ((currWire->name != "Clk") && (currWire->name != "Rst")))
+            // Update critical path
+            criticalPath = max(pathDelay, criticalPath);
+        }
+    }
+
+    // Loop through all the components in the data manager
+    for (component*& currComponent : data_manager->components)
+    {
+        // Consider only register components
+        if (currComponent->type == ComponentType::REG)
+        {
+            // Get maximum path delay starting at register
+            // Delay of register component included at beginning of paths only
+            double pathDelay = getComponentDelay(currComponent);
+            pathDelay += computeCriticalPath(currComponent->outputs["q"]->connection);
+            
+            // Update critical path
+            criticalPath = max(pathDelay, criticalPath);
+        }
+    }
+    return criticalPath;
+}
+
+// Function recursively computes critical path starting at a wire
+double CriticalPathCalculator::computeCriticalPath(wire* currWire)
+{
+    // Initialize critical path
+    double criticalPath = 0.0;
+
+    // Return critical path of 0 for outputs
+    if (currWire->type == WireType::OUTPUT) {
+        return 0.0;
+    }
+
+    // Loop through each of the ports the wire is connected to
+    for (port*& currPort : currWire->dest)
+    {
+        // Determine component corresponding to each port
+        component* currComponent = currPort->parent;
+
+        // Terminate at registers without considering register delay
+        // Delay of registers applied at beginning of paths only
+        if (currComponent->type != ComponentType::REG){
+
+            // Determine the component delay 
+            double componentDelay = getComponentDelay(currComponent);
+            
+            // Initialize max delay for component output
+            double maxPathDelay = 0.0;
+
+            // Determine the maximum delay for each of the components outputs
+            auto begin = currComponent->outputs.begin();
+            auto end = currComponent->outputs.end();
+            for (auto it = begin; it != end; ++it)
             {
-                // Compute maximum path delay starting at input
-                double pathDelay = computeCriticalPath(currWire);
-
-                // Update critical path
-                criticalPath = max(pathDelay, criticalPath);
+                wire* nextWire = it->second->connection;
+                double pathDelay = computeCriticalPath(nextWire);
+                maxPathDelay = max(pathDelay, maxPathDelay);
             }
-        }
 
-         // Loop through all the components in the data manager
-        for (component*& currComponent : data_manager->components)
-        {
-            // Consider only register components
-            if (currComponent->type == ComponentType::REG)
-            {
-                // Get maximum path delay starting at register
-                // Delay of register component included at beginning of paths only
-                double pathDelay = getComponentDelay(currComponent);
-                pathDelay += computeCriticalPath(currComponent->outputs["q"]->connection);
-                
-                // Update critical path
-                criticalPath = max(pathDelay, criticalPath);
-            }
+            // Update the path delay with the delay of the current component
+            maxPathDelay += componentDelay;
+
+            // Update the critical path
+            criticalPath = max(maxPathDelay, criticalPath);
         }
-        return criticalPath;
     }
+    return criticalPath;
+}
 
-    // Function recursively computes critical path starting at a wire
-    double CriticalPathCalculator::computeCriticalPath(wire* currWire)
+// Function computes the delay of a component
+double CriticalPathCalculator::getComponentDelay(component* currComponent)
+{
+    // Get row in table corresponding to component
+    int componentIdx = ComponentTypeToInt(currComponent->type);
+
+    // Get column in table corresponding to width
+    int widthIdx = -1;
+    switch (currComponent->width)
     {
-        // Initialize critical path
-        double criticalPath = 0.0;
-
-        // Return critical path of 0 for outputs
-        if (currWire->type == WireType::OUTPUT) {
-            return 0.0;
-        }
-
-        // Loop through each of the ports the wire is connected to
-        for (port*& currPort : currWire->dest)
-        {
-            // Determine component corresponding to each port
-            component* currComponent = currPort->parent;
-
-            // Terminate at registers without considering register delay
-            // Delay of registers applied at beginning of paths only
-            if (currComponent->type != ComponentType::REG){
-
-                // Determine the component delay 
-                double componentDelay = getComponentDelay(currComponent);
-                
-                // Initialize max delay for component output
-                double maxPathDelay = 0.0;
-
-                // Determine the maximum delay for each of the components outputs
-                auto begin = currComponent->outputs.begin();
-                auto end = currComponent->outputs.end();
-                for (auto it = begin; it != end; ++it)
-                {
-                    wire* nextWire = it->second->connection;
-                    double pathDelay = computeCriticalPath(nextWire);
-                    maxPathDelay = max(pathDelay, maxPathDelay);
-                }
-
-                // Update the path delay with the delay of the current component
-                maxPathDelay += componentDelay;
-
-                // Update the critical path
-                criticalPath = max(maxPathDelay, criticalPath);
-            }
-        }
-        return criticalPath;
+        case 1:
+            widthIdx = 0;
+            break;
+        case 2:
+            widthIdx = 1;
+            break;
+        case 8:
+            widthIdx = 2;
+            break;
+        case 16:
+            widthIdx = 3;
+            break;
+        case 32:
+            widthIdx = 4;
+            break;
+        case 64:
+            widthIdx = 5;
+            break;
     }
 
-    // Function computes the delay of a component
-    double CriticalPathCalculator::getComponentDelay(component* currComponent)
-    {
-        // Get row in table corresponding to component
-        int componentIdx = ComponentTypeToInt(currComponent->type);
-
-        // Get column in table corresponding to width
-        int widthIdx = -1;
-        switch (currComponent->width)
-        {
-            case 1:
-                widthIdx = 0;
-                break;
-            case 2:
-                widthIdx = 1;
-                break;
-            case 8:
-                widthIdx = 2;
-                break;
-            case 16:
-                widthIdx = 3;
-                break;
-            case 32:
-                widthIdx = 4;
-                break;
-            case 64:
-                widthIdx = 5;
-                break;
-        }
-
-        // Return delay from table
-        return COMPONENT_DELAY[componentIdx][widthIdx];
-    }
+    // Return delay from table
+    return COMPONENT_DELAY[componentIdx][widthIdx];
+}
 } // namespace DataPathGen
--- src/FileWriter.cpp
+++ src/FileWriter.cpp
@@ -6,18 +6,29 @@
 
 namespace DataPathGen
 {
+    // Class constructor provides access to shared data manager
     FileWriter::FileWriter(DataManager* data_manager)
-        : data_manager(data_manager)
-        {}
+        : data_manager(data_manager) {}
+
+    // Function determines the module name
     void FileWriter::determineModuleName(string filePath)
     {
+        // Strip file path and file extension from input file
+        // find_last_of will return -1 for non-matches
         int startIdx = filePath.find_last_of("/\\") + 1;
         int endIdx = filePath.find_last_of(".");
+
+        // Take all characters if there is no file extension
         if (endIdx < 0)
         {
             endIdx = filePath.length();
         }
+
+        // Extract file name from file path
         string fileName = filePath.substr(startIdx, endIdx-startIdx);
+
+        // Remove the 474a_ and 574a_ substrings from module name
+        // Module name should not start with numbers
         if ((fileName.substr(0,5) == "474a_") || (fileName.substr(0,5) == "574a_"))
         {
             moduleName = fileName.substr(5);
@@ -27,17 +38,25 @@ namespace DataPathGen
             moduleName = fileName;
         }
     }
+
+    // Function declares the timescale at the beginning of the file
     void FileWriter::declareTimescale()
     {
         circuitFile << "`timescale 1ns/1ns" << endl << endl;
     }
+
+    // Function declares the module at the beginning of the file
     void FileWriter::declareModule()
     {
         circuitFile << "module " << moduleName;
         circuitFile << "(";
         bool firstInput = true;
+
+        // Loops through all the wires in the data manager
         for (wire*& currWire : data_manager->wires)
         {
+
+            // Print input and output ports
             if ((currWire->type == WireType::INPUT) || (currWire->type == WireType::OUTPUT))
             {
                 if (!firstInput)
@@ -50,12 +69,17 @@ namespace DataPathGen
         }
         circuitFile << ");" << endl << endl;
     }
+
+    // Function terminates the module
     void FileWriter::terminateModule()
     {
         circuitFile << "endmodule" << endl;
     }
+
+    // Function declares a net (input, output, or wire)
     void FileWriter::declareNet(wire* net)
     {
+        // Declare net type
         if (net->type == WireType::INPUT)
         {
             circuitFile << "    input ";
@@ -68,60 +92,98 @@ namespace DataPathGen
         {
             circuitFile << "    wire ";
         }
+
+        // Declare net sign
         if (net->sign)
         {
             circuitFile << "signed ";
         }
+
+        // Declare net range
         if (net->width > 1)
         {
             circuitFile << "[" << net->width - 1 << ":0] ";
         }
+
+        // Terminate with a semicolon
         circuitFile << net->name << ";" << endl;
     }
+
+    // Function declares inputs
     void FileWriter::declareInputs()
     {
+        // Loop through all the wires in the data manager
         for (wire*& currWire : data_manager->wires)
         {
+            // Declare all inputs
             if (currWire->type == WireType::INPUT)
             {
                 declareNet(currWire);
             }
         }
+
+        // Add a line of whitespace after the inputs
         circuitFile << endl;
     }
+
+    // Function declares outputs
     void FileWriter::declareOutputs()
     {
+        // Loop through all the wires in the data manager
         for (wire*& currWire : data_manager->wires)
         {
+            // Declare all outputs
             if (currWire->type == WireType::OUTPUT)
             {
                 declareNet(currWire);
             }
         }
+
+        // Add a line of whitespace after the outputs
         circuitFile << endl;
     }
+
+    // Function declares wires
     void FileWriter::declareWires()
     {
+        // Loop through all the wires in the data manager
         for (wire*& currWire : data_manager->wires)
         {
+            // Declare all wires and registers. Registers are declared as wires
+            // because they are driven from a component instead of an always block
             if ((currWire->type == WireType::WIRE) || (currWire->type == WireType::REGISTER))
             {
                 declareNet(currWire);
             }
         }
+
+        // Add a line of whitespace after the wires
         circuitFile << endl;
     }
+
+    // Function declares cast components
     void FileWriter::declareCasts()
     {
+        // Flag specifies whether at least one cast component exists
         bool atLeastOneCast = false;
+
+        // Loop through all components in the data manager
         for (component*& currComponent : data_manager->components)
         {
+            // If component is a cast
             if (currComponent->type == ComponentType::CAST)
             {
+                // Update flag
                 atLeastOneCast = true;
+
+                // Get input and output of each cast
                 wire* output = currComponent->outputs["out"]->connection;
                 wire* input = currComponent->inputs["in"]->connection;
+
+                // Create assign statement for cast
                 circuitFile << "    assign " << output->name << " = ";
+
+                // Handle sign change
                 if (output->sign != input->sign)
                 {
                     if (output->sign)
@@ -133,29 +195,40 @@ namespace DataPathGen
                         circuitFile << "$unsigned(";
                     }
                 }
+
+                // Handle width change
                 if (output->width != input->width)
                 {
+                    // Smaller width to larger width
                     if (output->width > input->width)
                     {
+                        // Determine the number of bits to add
                         int numBitsToAdd = output->width - input->width;
+
+                        // Sign extend
                         if (input->sign)
                         {
                             circuitFile << "{{" << numBitsToAdd << "{" << input->name << "[" << input->width - 1 << "]}}, " << input->name << "}";
                         }
+                        // Zero pad
                         else
                         {
                             circuitFile << "{{" << numBitsToAdd << "{1'b0}}, " << input->name << "}";
                         }
                     }
+                    // Larger width to smaller width
                     else
                     {
                         circuitFile << input->name << "[" << output->width-1 << ":0]";
                     }
                 }
+                // No width change
                 else
                 {
                     circuitFile << input->name;
                 }
+
+                // Terminate sign change
                 if (output->sign != input->sign)
                 {
                     circuitFile << ");" << endl;
@@ -166,11 +239,15 @@ namespace DataPathGen
                 }
             }
         }
+
+        // Only add a newline if there is at least one cast
         if (atLeastOneCast)
         {
             circuitFile << endl;
         }
     }
+
+    // Function declares components
     void FileWriter::declareComponents()
     {
         for (component*& currComponent : data_manager->components)
